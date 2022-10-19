539889ed27301a7f4a9f5ede699b08b34c44ac47
Lesley Keyes <lesleyjkeyes@gmail.com>
2022-10-18 21:33:24 -0500

    finished chapter 6

---
+++ kennels-server/views/customer_request.py
@@ -0,0 +1,60 @@
+CUSTOMERS = [
+        {
+            "id": 1,
+            "name": "Anne Smith",
+        },
+        {
+            "id": 2,
+            "name": "George Wu",
+        },
+        {
+            "id": 3,
+            "name": "Jose Gomez",
+        },
+        {
+            "id": 4,
+            "name": "Andrea Mathis",
+        }
+]
+
+def get_all_customers():
+    """Returns all customers"""
+    return CUSTOMERS
+
+# Function with a single parameter
+def get_single_customer(id):
+    """returns a single location"""
+    requested_customer = None
+
+    for customer in CUSTOMERS:
+        if customer["id"] == id:
+            requested_customer = customer
+
+    return requested_customer
+
+def create_customer(customer):
+    max_id = CUSTOMERS[-1]["id"]
+
+    new_id = max_id + 1
+
+    customer["id"] = new_id
+
+    CUSTOMERS.append(customer)
+
+    return customer
+
+def delete_customer(id):
+    customer_index = -1
+
+    for index, customer in enumerate(CUSTOMERS):
+        if customer["id"] == id:
+            customer_index = index
+
+    if customer_index >= 0:
+        CUSTOMERS.pop(customer_index)
+
+def update_customer(id, new_customer):
+    for index, customer in enumerate(CUSTOMERS):
+        if customer["id"] == id:
+            CUSTOMERS[index] = new_customer
+            break
